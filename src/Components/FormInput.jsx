bc185a60189188cc82d6deea78b2a1c72859b133
tusharbhatt101 <102147917+tusharbhatt101@users.noreply.github.com>
2024-03-01 19:42:01 +0530

    Update FormInput.jsx
    updation

--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -3,7 +3,7 @@ import "./formInput.css";
 
 const FormInput = (props) => {
   const [focused, setFocused] = useState(false);
-  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  const { label, errorMessage, onChange, options, type, id, ...inputProps } = props;
 
   const handleFocus = (e) => {
     setFocused(true);
@@ -12,15 +12,42 @@ const FormInput = (props) => {
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input
-        {...inputProps}
-        onChange={(e) => onChange({ target: { name: inputProps.name, value: e.target.value } })}
-        onBlur={handleFocus}
-        onFocus={() =>
-          inputProps.name === "confirmPassword" && setFocused(true)
-        }
-        focused={focused.toString()}
-      />
+      {type === "dropdown" ? (
+        <select
+          {...inputProps}
+          value={inputProps.value}
+          onChange={(e) =>
+            onChange({
+              target: { name: inputProps.name, value: e.target.value },
+            })
+          }
+          onBlur={handleFocus}
+          onFocus={handleFocus}
+        >
+          <option value="" disabled>
+            Select {label}
+          </option>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input
+          {...inputProps}
+          onChange={(e) =>
+            onChange({
+              target: { name: inputProps.name, value: e.target.value },
+            })
+          }
+          onBlur={handleFocus}
+          onFocus={() =>
+            inputProps.name === "confirmPassword" && setFocused(true)
+          }
+          focused={focused.toString()}
+        />
+      )}
       <span>{errorMessage}</span>
     </div>
   );
